26b45bade65f0738926250d696d777587b8a29f9
zhizhonghua <zhizhonghua@boco.com.cn>
2019-05-13 22:15:29 +0800

    调整代码逻辑 增加时间输出.

--- src/main/java/net/cc/demo_springcloud_eureka_client/web/controller/HelloController.java
+++ src/main/java/net/cc/demo_springcloud_eureka_client/web/controller/HelloController.java
@@ -1,7 +1,12 @@
 package net.cc.demo_springcloud_eureka_client.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @classname HelloController
@@ -13,8 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello() {
-        return "hello";
+
+        String str = String.format("Date [%s]  Msg [%s]", getSysDate(), "Hello");
+        System.out.println(str);
+
+        return str;
+    }
+
+
+    /**
+     * 功能描述:
+     * 获取系统当前时间
+     *
+     * @param
+     * @return: java.lang.String
+     * @exception:
+     * @author: zhonghua.zhi
+     * @date: 2019-05-13 22:01
+     */
+    protected String getSysDate() {
+
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return df.format(new Date());
     }
 }
